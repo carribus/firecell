1ed4c6b32374a172659440b2ba93a72c91df2dc2
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2008-10-21 14:07:19 +0000

    Updates to the UserManager object

--- trunk/fcadmin/apps/usermanager.php
+++ trunk/fcadmin/apps/usermanager.php
@@ -9,11 +9,13 @@ class User
   public $account_password;
   public $account_created;
   public $account_type;
+  public $account_email;
 }
 
 class UserManagerApp implements IFCAdminApp
 {
   // attributes
+  private $userAction = NULL;
   private $itemsPerPage = 50;
   private $db = NULL;
   private $currentPage = 0;
@@ -42,25 +44,39 @@ class UserManagerApp implements IFCAdminApp
     
     $this->db = $db;
     
-    // check if a specific page was requested (if not, set it to 0)
-    $this->currentPage = $_GET["page"];
-    if ( $this->currentPage == NULL )
-      $this->currentPage = 0;
-
-    // get the number of users so we can calculate the pagination
-    $this->numUsers = $this->GetNumUsers();                    
-    if ( $this->numUsers == -1 )
-    {
-      echo "Failed to retrieve number of users!";
-      return false;
-    }              
-    // calculate the number of pages available
-    $this->numPagesAvailable = ceil($this->numUsers / $this->itemsPerPage);
-      
-    // get the user data
-    $this->userData = $this->GetUsers();
+    $this->userAction = $_GET["u"];
     
-    $userCount = count($this->userData);
+    if ( $this->userAction == NULL )
+    {
+      // check if a specific page was requested (if not, set it to 0)
+      $this->currentPage = $_GET["page"];
+      if ( $this->currentPage == NULL )
+        $this->currentPage = 0;
+
+      // get the number of users so we can calculate the pagination
+      $this->numUsers = $this->GetNumUsers();                    
+      if ( $this->numUsers == -1 )
+      {
+        echo "Failed to retrieve number of users!";
+        return false;
+      }              
+      // calculate the number of pages available
+      $this->numPagesAvailable = ceil($this->numUsers / $this->itemsPerPage);
+        
+      // get the user data
+      $this->userData = $this->GetUsers();
+    }    
+    else
+    {
+      switch ( $this->userAction )
+      {
+        case  -1:
+          break;
+          
+        default:
+          break;
+      }
+    }
       
     return true;
   }
@@ -69,11 +85,39 @@ class UserManagerApp implements IFCAdminApp
   
   public function render()
   {
-    $startIndex = $this->currentPage * $this->itemsPerPage;
-    
-    
+    if ( $this->userAction == NULL )
+    {
+      $startIndex = $this->currentPage * $this->itemsPerPage;
+      $this->render_options();
+      $this->render_table($startIndex);    
+      $this->render_pages();
+    }
+    else if ( $this->userAction == -1 )
+    {
+      // add a new user
+      echo "Adding a new user not implemented yet";
+    }
+    else
+    {
+      // display the user's details for modification
+      echo "Editing for users not implemented yet (would be editing user id ".$this->userAction;
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////
+  
+  private function render_options()
+  {
+    echo "<a href=\"?a=users&u=-1\">Add User</a>";
+    echo "<br/><br/>";
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////
+
+  private function render_table($startIndex)
+  {
     echo "<table class=\"tableUsers\" cellspacing=\"0\">";
-    echo "<tr><th>ID</th><th>Account Name</th><th>Password</th><th>Type</th><th>Created</th>";
+    echo "<tr><th>ID</th><th>Account Name</th><th>Password</th><th>Type</th><th>Email</th><th>Created</th>";
     for ( $i = $startIndex; $i < $this->numUsers; $i++ )
     {
       $user = $this->userData[$i];
@@ -81,13 +125,35 @@ class UserManagerApp implements IFCAdminApp
       echo "<td><a href=\"?a=users&u=".$user->account_id."\">".$user->account_name."</a></td>";
       echo "<td>******</td>";
       echo "<td>".$user->account_type."</td>";
+      echo "<td>".$user->account_email."</td>";
       echo "<td>".$user->account_created."</td>";
       echo "</tr>";
     }
     echo "</table>";
-    echo "<br/>".$i." users listed";
+    echo "<small>".($i-$startIndex)." users listed</small><br/><br/>";
   }
+  
+  /////////////////////////////////////////////////////////////////////////////////
 
+  private function render_pages()
+  {
+    echo "<center><small>Pages</small><br/>";
+    for ( $i = 0; $i < $this->numPagesAvailable; $i++ )
+    {
+      if ( $i != $this->currentPage )
+      {
+        echo "<a href=\"?a=users&page=$i\">";
+      }
+      echo ($i+1);
+      if ( $i != $this->currentPage )
+      {
+        echo "</a>";
+      }
+      echo "&nbsp;&nbsp;";
+    }
+    echo "</center>";
+  }
+  
   /////////////////////////////////////////////////////////////////////////////////
   
   private function GetNumUsers()
@@ -128,6 +194,7 @@ class UserManagerApp implements IFCAdminApp
         $user->account_password = $line["acc_password"];
         $user->account_created = $line["created"];
         $user->account_type = $line["acc_type"];
+        $user->account_email = $line["email"];
         
         array_push( $userData, $user );
       }      
