5fa8e73aee451ee3bd8b8c42b49a5446a2288723
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2008-10-30 09:45:12 +0000

    Completed 1st part of refactor+abstraction of service logic object Next phase is to move the Auth service over to derive from ServiceLogicBase and implement the DB handler hooks into the base class **Added the mysql includes and lib files under /common/mysql

--- FCWorldServer/FCLogicWorld.h
+++ FCWorldServer/FCLogicWorld.h
@@ -23,46 +23,15 @@
 #include <string>
 #include <map>
 #include <queue>
-#include "../common/binstream.h"
-#include "../common/threading.h"
-#include "../common/fctypes.h"
-#include "../common/PEPacket.h"
-#include "../common/PacketExtractor.h"
 #include "../common/ServiceLogicBase.h"
-#include "../Clients/common/Socket/ClientSocket.h"
 
 class FCLogicWorld : public ServiceLogicBase
-                   , public IBaseSocketSink
-
 {
-  class RouterSocket : public BaseSocket
-  {
-  public:
-    RouterSocket() {}
-    ~RouterSocket() {}
-
-    void SetServer(string server)             { m_server = server; }
-    string GetServer()                        { return m_server; }
-    void SetPort(short port)                  { m_port = port; }
-    short GetPort()                           { return m_port; }
-    void AddData(FCBYTE* pData, FCULONG len)
-    {
-      m_stream.Concat(pData, len);
-    }
-    CBinStream<FCBYTE, true>& GetDataStream() { return m_stream; }
-
-  private:
-    string              m_server;
-    short               m_port;
-    CBinStream<FCBYTE, true> m_stream;
-  };
-
-  typedef std::map< string, RouterSocket* > ServiceSocketMap;
-  typedef std::queue<FCSOCKET> CQueuedSocketArray;
-
 public:
   FCLogicWorld(void);
   ~FCLogicWorld(void);
+
+  ServiceType GetServiceType()                    { return ST_World; }
 
   //
   // IServiceLogic implementation
@@ -70,31 +39,11 @@ public:
   int Start();
   int Stop();
 
-  //
-  // IBaseSocketSink implementation
-  void OnAccepted(BaseSocket* pSocket, int nErrorCode) {}
-	void OnConnected(BaseSocket* pSocket, int nErrorCode);
-	void OnDisconnected(BaseSocket* pSocket, int nErrorCode);
-	void OnDataReceived(BaseSocket* pSocket, FCBYTE* pData, int nLen);
-
 private:
 
-  void RegisterServiceWithRouter(RouterSocket* pSock);
-
-  void HandlePacket(PEPacket* pPkt, BaseSocket* pSocket);
-  bool ConnectToRouters();
-  void DisconnectFromRouters();
-/*
-  bool ConfigureDatabase();
-  static void* thrdDBWorker(void* pData);
-  void HandleCompletedDBJob(FCDBJob& job);
-*/
   bool OnCommand(PEPacket* pPkt, BaseSocket* pSocket);
   bool OnResponse(PEPacket* pPkt, BaseSocket* pSocket);
   bool OnError(PEPacket* pPkt, BaseSocket* pSocket);
-
-  ServiceSocketMap m_mapRouters;
-  PacketExtractor m_pktExtractor;
 };
 
 #endif//_FCLOGICWORLD_H_
--- common/ServiceLogicBase.h
+++ common/ServiceLogicBase.h
@@ -2,15 +2,59 @@
 #define _SERVICELOGICBASE_H_
 
 #include <string>
+#include <map>
+#include "binstream.h"
+#include "threading.h"
+#include "fctypes.h"
 #include "inifile.h"
-#include "../common/database/FCDatabase.h"
 #include "interfaces/IServiceLogic.h"
+#include "protocol/fcprotocol.h"
+#include "PEPacket.h"
+#include "PacketExtractor.h"
+#include "Database/FCDatabase.h"
+#include "../Clients/common/Socket/ClientSocket.h"
 
 using namespace std;
 
 class ServiceLogicBase : public IServiceLogic
+                       , public IBaseSocketSink
 {
 public:
+
+  class RouterSocket : public BaseSocket
+  {
+  public:
+    RouterSocket() {}
+    ~RouterSocket() {}
+
+    void SetServer(string server)             { m_server = server; }
+    string GetServer()                        { return m_server; }
+    void SetPort(short port)                  { m_port = port; }
+    short GetPort()                           { return m_port; }
+    void AddData(FCBYTE* pData, FCULONG len)
+    {
+      m_stream.Concat(pData, len);
+    }
+    CBinStream<FCBYTE, true>& GetDataStream() { return m_stream; }
+
+  private:
+    string              m_server;
+    short               m_port;
+    CBinStream<FCBYTE, true> m_stream;
+  };
+  typedef std::map< string, RouterSocket* > ServiceSocketMap;
+  typedef std::queue<FCSOCKET> CQueuedSocketArray;
+
+/*
+  struct DBJobContext
+  {
+    RouterSocket* pRouter;
+    FCSOCKET clientSocket;
+  };
+*/
+  // the void* param is the optional context data that each derived class will pass in on the FCDatabase::ExecuteJob() call
+  typedef void (*DBHANDLERPROC)(DBIResultSet&, void*);
+
   ServiceLogicBase(const string& serviceName, bool bHasConsole);
   virtual ~ServiceLogicBase(void);
 
@@ -20,23 +64,47 @@ public:
   const char* GetName();
   void HasConsole(bool bHasConsole)               { m_bHasConsole = bHasConsole; }
 
+  //
+  // IBaseSocketSink implementation
+  void OnAccepted(BaseSocket* pSocket, int nErrorCode) {}
+	void OnConnected(BaseSocket* pSocket, int nErrorCode);
+	void OnDisconnected(BaseSocket* pSocket, int nErrorCode);
+	void OnDataReceived(BaseSocket* pSocket, FCBYTE* pData, int nLen);
+
+  virtual ServiceType GetServiceType() = 0;
+
 protected:
+
+  virtual bool OnCommand(PEPacket* pPkt, BaseSocket* pSocket) = 0;
+  virtual bool OnResponse(PEPacket* pPkt, BaseSocket* pSocket) = 0;
+  virtual bool OnError(PEPacket* pPkt, BaseSocket* pSocket) = 0;
 
   bool HasConsole()                               { return m_bHasConsole; }
 
   bool LoadConfig(FCCSTR strFilename);
   bool LoadDBSettingsFromConfig(string& strEngine, string& strServer, string& strDBName, string& strUser, string& strPass);
 
+  bool ConnectToRouters();
+  void DisconnectFromRouters();
+
   bool ConfigureDatabase(string strEngine, string strServer, string strDBName, string strUser, string strPass);
-  static void* thrdDBWorker(void* pData);
-  void HandleCompletedDBJob(FCDBJob& job);
+  void RegisterDBHandler(const string strJobRef, DBHANDLERPROC handler);
 
   /*
    *  Protected members
    */
   INIFile m_config;
+  ServiceSocketMap m_mapRouters;
+  PacketExtractor m_pktExtractor;
 
 private:
+
+  void RegisterServiceWithRouter(RouterSocket* pSock);
+
+  void HandlePacket(PEPacket* pPkt, BaseSocket* pSocket);
+
+  static void* thrdDBWorker(void* pData);
+  void HandleCompletedDBJob(FCDBJob& job);
 
   string m_ServiceName;
   bool m_bHasConsole;
@@ -45,6 +113,10 @@ private:
   FCDatabase m_db;
   pthread_t m_thrdDBMon;
   bool m_bDBMonRunning;
+
+  // db proc handlers
+  typedef map<string, DBHANDLERPROC> MapDBHandlers;
+  MapDBHandlers m_mapDBHandlers;
 };
 
 #endif//_SERVICELOGICBASE_H_
