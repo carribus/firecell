2086e1ca0d3afb6b0c597cf0f9765b1df821202c
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2008-12-31 14:08:18 +0000

    Big Update: - Added start of the real client. It is being written using the Irrlicht engine. The redistribution dll is included in the debug/bin/client folder - Restructured the /bin folder to seperate the client and server binaries and configurations - Some more work on the forum code: Forum posts now have the ability to be locked.

---
+++ trunk/Clients/FCClient/FCModel.cpp
@@ -0,0 +1,42 @@
+#include "FCModel.h"
+
+FCModel::FCModel(void)
+{
+}
+
+///////////////////////////////////////////////////////////////////////
+
+FCModel::~FCModel(void)
+{
+}
+
+///////////////////////////////////////////////////////////////////////
+
+void FCModel::SubscribeToEvents(IModelEventSink* pSink)
+{
+	if ( !IsSubscribed(pSink) )
+		m_sinks.push_back(pSink);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+bool FCModel::IsSubscribed(IModelEventSink* pSink)
+{
+	vector<IModelEventSink*>::iterator it = m_sinks.begin();
+
+	while ( it != m_sinks.end() )
+	{
+		if ( (*it) == pSink )
+			return true;
+		it++;
+	}
+
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////////
+
+bool FCModel::Initialise()
+{
+	return false;
+}
---
+++ trunk/Clients/FCClient/FCView.h
@@ -0,0 +1,39 @@
+#ifndef _FCVIEW_H_
+#define _FCVIEW_H_
+
+#include <irrlicht.h>
+#include "IModelEventSink.h"
+#include "FCModel.h"
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+class FCController;
+class FCView : public IModelEventSink
+{
+public:
+	FCView(void);
+	~FCView(void);
+
+	void SetModel(FCModel* pModel);
+	void SetController(FCController* pController);
+
+	bool Initialise(E_DRIVER_TYPE driverType = EDT_OPENGL);
+
+	void OnModelEvent() {}
+
+private:
+
+	FCModel*					m_pModel;
+	FCController*			m_pController;
+
+	IrrlichtDevice*		m_pDevice;
+	IVideoDriver*			m_pDriver;
+	IGUIEnvironment*	m_pEnv;
+};
+
+#endif//_FCVIEW_H_
