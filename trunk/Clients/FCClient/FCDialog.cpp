0b860cff3eb3e951c55e04f4a577b171f43b6e8f
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2009-02-01 22:52:52 +0000

    Added FCDialog base class for irrLicht based dialog objects. Still need to add nice mechanism for adding form elements

---
+++ trunk/Clients/FCClient/FCDialog.cpp
@@ -0,0 +1,278 @@
+#include "FCDialog.h"
+
+FCDialog::FCDialog(IGUIEnvironment* env, IGUIElement* pParent, wchar_t* caption, bool bModal, s32 id)
+: IGUIElement(EGUIET_ELEMENT, env, pParent ? pParent : env->getRootGUIElement(), id, core::rect<s32>(0, 0, 640, 480))
+, m_bModal(bModal)
+, Dragging(false)
+, m_fpSuccessCallback(NULL)
+, m_fpCancelCallback(NULL)
+{
+	IGUISkin* skin = Environment->getSkin();
+	IGUISpriteBank* sprites = 0;
+	video::SColor color(255,255,255,255);
+	if (skin)
+	{
+		sprites = skin->getSpriteBank();
+		color = skin->getColor(EGDC_WINDOW_SYMBOL);
+	}
+
+	s32 buttonw = Environment->getSkin()->getSize(EGDS_WINDOW_BUTTON_WIDTH);
+	s32 posx = RelativeRect.getWidth() - buttonw - 4;
+
+	m_CloseButton = Environment->addButton(core::rect<s32>(posx, 3, posx + buttonw, 3 + buttonw), this, -1,
+		L"", skin ? skin->getDefaultText(EGDT_WINDOW_CLOSE) : L"Close");
+	m_CloseButton->setSubElement(true);
+	m_CloseButton->setTabStop(false);
+	if (sprites)
+	{
+		m_CloseButton->setSpriteBank(sprites);
+		m_CloseButton->setSprite(EGBS_BUTTON_UP, skin->getIcon(EGDI_WINDOW_CLOSE), color);
+		m_CloseButton->setSprite(EGBS_BUTTON_DOWN, skin->getIcon(EGDI_WINDOW_CLOSE), color);
+	}
+	m_CloseButton->setAlignment(EGUIA_LOWERRIGHT, EGUIA_LOWERRIGHT, EGUIA_UPPERLEFT, EGUIA_UPPERLEFT);
+	m_CloseButton->grab();
+
+	Text = caption;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+FCDialog::~FCDialog(void)
+{
+	if (m_CloseButton)
+		m_CloseButton->drop();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void FCDialog::draw()
+{
+	if (!IsVisible)
+		return;
+
+	IGUISkin* skin = Environment->getSkin();
+
+	core::rect<s32> rect = AbsoluteRect;
+	core::rect<s32> *cl = &AbsoluteClippingRect;
+
+	// draw body fast
+	rect = skin->draw3DWindowBackground(this, true, skin->getColor(EGDC_ACTIVE_BORDER),
+		AbsoluteRect, &AbsoluteClippingRect);
+
+	if (Text.size())
+	{
+		rect.UpperLeftCorner.X += skin->getSize(EGDS_TEXT_DISTANCE_X);
+		rect.UpperLeftCorner.Y += skin->getSize(EGDS_TEXT_DISTANCE_Y);
+		rect.LowerRightCorner.X -= skin->getSize(EGDS_WINDOW_BUTTON_WIDTH) + 5;
+
+		IGUIFont* font = skin->getFont(EGDF_WINDOW);
+		if (font)
+			font->draw(Text.c_str(), rect, skin->getColor(EGDC_ACTIVE_CAPTION), false, true, cl);
+	}
+
+	IGUIElement::draw();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool FCDialog::OnEvent(const SEvent& event)
+{
+	if (IsEnabled)
+	{
+		switch(event.EventType)
+		{
+		case EET_GUI_EVENT:
+			{
+				switch(event.GUIEvent.EventType)
+				{
+				case EGET_ELEMENT_FOCUSED:
+					// only children are allowed the focus
+					if (event.GUIEvent.Caller != this && !isMyChild(event.GUIEvent.Caller))
+						Environment->setFocus(this);
+					return false;
+				case EGET_ELEMENT_FOCUS_LOST:
+					// only children are allowed the focus
+					if (!(isMyChild(event.GUIEvent.Element) || event.GUIEvent.Element == this))
+					{
+						return true;
+					}
+					else
+					{
+						return IGUIElement::OnEvent(event);
+					}
+					break;
+
+				case	EGET_CHECKBOX_CHANGED:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUICheckBox* pCB = (IGUICheckBox*) event.GUIEvent.Caller;
+							return OnCheckboxChanged(event.GUIEvent.Caller->getID(), pCB);
+						}
+					}
+					break;
+
+				case	EGET_COMBO_BOX_CHANGED:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUIComboBox* pCB = (IGUIComboBox*) event.GUIEvent.Caller;
+							return OnComboBoxChanged(event.GUIEvent.Caller->getID(), pCB);
+						}
+					}
+					break;
+
+				case	EGET_EDITBOX_ENTER:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUIEditBox* pEdit = (IGUIEditBox*) event.GUIEvent.Caller;
+							return OnEditBoxEnter(event.GUIEvent.Caller->getID(), pEdit);
+						}
+					}
+					break;
+
+				case	EGET_LISTBOX_CHANGED:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUIListBox* pLB = (IGUIListBox*) event.GUIEvent.Caller;
+							return OnListBoxChanged(event.GUIEvent.Caller->getID(), pLB);
+						}
+					}
+					break;
+
+				case	EGET_LISTBOX_SELECTED_AGAIN:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUIListBox* pLB = (IGUIListBox*) event.GUIEvent.Caller;
+							return OnListBoxSelectedAgain(event.GUIEvent.Caller->getID(), pLB);
+						}
+					}
+					break;
+
+				case	EGET_SCROLL_BAR_CHANGED:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUIScrollBar* pSB = (IGUIScrollBar*) event.GUIEvent.Caller;
+							return OnScrollBarChanged(event.GUIEvent.Caller->getID(), pSB);
+						}
+					}
+					break;
+
+				case	EGET_SPINBOX_CHANGED:
+					{
+						if ( isMyChild(event.GUIEvent.Caller) )
+						{
+							IGUISpinBox* pSB = (IGUISpinBox*) event.GUIEvent.Caller;
+							return OnSpinBoxChanged(event.GUIEvent.Caller->getID(), pSB);
+						}
+					}
+					break;
+
+				case EGET_BUTTON_CLICKED:
+					{
+						// check if the window close button was clicked
+						if ( isMyChild(event.GUIEvent.Caller) ) 
+						{
+							if (event.GUIEvent.Caller == m_CloseButton )
+							{
+								if ( m_fpCancelCallback )
+									(*m_fpCancelCallback)();
+								closeDialog();
+								delete this;
+								return true;
+							}
+
+							// if not... lets pass the event on if its one of our children
+							IGUIButton* pBtn = (IGUIButton*) event.GUIEvent.Caller;
+							return OnButtonClicked(event.GUIEvent.Caller->getID(), pBtn);
+						}
+					}
+					break;
+
+				case EGET_ELEMENT_CLOSED:
+					// do not interfere with children being removed
+					return IGUIElement::OnEvent(event);
+
+				default:
+					break;
+				}
+			}
+			break;
+
+		case EET_MOUSE_INPUT_EVENT:
+			{
+				switch(event.MouseInput.Event)
+				{
+				case EMIE_MOUSE_WHEEL:
+					break;
+
+				case EMIE_LMOUSE_PRESSED_DOWN:
+					if ( isPointInside( position2d<s32>( event.MouseInput.X, event.MouseInput.Y ) ) )
+					{
+						DragStart.X = event.MouseInput.X;
+						DragStart.Y = event.MouseInput.Y;
+						Dragging = true;
+					}
+					Environment->setFocus(this);
+					return true;
+
+				case EMIE_LMOUSE_LEFT_UP:
+					Dragging = false;
+					return true;
+
+				case EMIE_MOUSE_MOVED:
+					if (Dragging)
+					{
+						// gui window should not be dragged outside its parent
+						if (Parent)
+						{
+							if (event.MouseInput.X < Parent->getAbsolutePosition().UpperLeftCorner.X +1 ||
+								event.MouseInput.Y < Parent->getAbsolutePosition().UpperLeftCorner.Y +1 ||
+								event.MouseInput.X > Parent->getAbsolutePosition().LowerRightCorner.X -1 ||
+								event.MouseInput.Y > Parent->getAbsolutePosition().LowerRightCorner.Y -1)
+							{
+								return true;
+							}
+						}
+
+						move(core::position2d<s32>(event.MouseInput.X - DragStart.X, event.MouseInput.Y - DragStart.Y));
+						DragStart.X = event.MouseInput.X;
+						DragStart.Y = event.MouseInput.Y;
+						return true;
+					}
+					break;
+
+				default:
+					if ( IsVisible )
+						return true;
+					break;
+				}
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return IGUIElement::OnEvent(event);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void FCDialog::closeDialog()
+{
+	SEvent event;
+	event.EventType = EET_GUI_EVENT;
+	event.GUIEvent.Caller = this;
+	event.GUIEvent.Element = 0;
+	event.GUIEvent.EventType = EGET_FILE_CHOOSE_DIALOG_CANCELLED;
+	Parent->OnEvent(event);
+
+	remove();
+}
