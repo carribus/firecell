e39af6afb8ebd84e97090b1a32f3a07e38a0b036
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2009-07-11 14:54:43 +0000

    Minor work done on banking window

--- trunk/Clients/FCClient/GUIBankView.cpp
+++ trunk/Clients/FCClient/GUIBankView.cpp
@@ -1,11 +1,36 @@
+#include <sstream>
 #include "GUIBankView.h"
 
 GUIBankView::GUIBankView(IGUIEnvironment* environment, core::rect<s32>& rect, IGUIElement* pParent, s32 id)
 : IGUIElement(EGUIET_ELEMENT, environment, pParent, id, rect)
+, m_pTxtDetailsLabels(NULL)
+, m_pTxtDetails(NULL)
 {
 #ifdef _DEBUG
   setDebugName("GUIBankView");
 #endif//_DEBUG
+
+  m_pTxtDetailsLabels = environment->addStaticText(L"", core::rect<s32>( 10, 10, rect.getWidth()/2, 80 ), false, true, this, -1, false);
+  m_pTxtDetails = environment->addStaticText(L"", core::rect<s32>( rect.getWidth()/2+1, 10, rect.getWidth()-10, 80 ), false, true, this, -1, false);
+
+	// setup the font
+	IGUIFont* pFont = environment->getFont("./clientdata/fonts/fontcourier.png");
+	environment->getSkin()->setFont(pFont);
+
+  m_pTxtDetailsLabels->setOverrideColor( SColor(255, 196, 196, 255) );
+	m_pTxtDetailsLabels->setOverrideFont(pFont);
+  m_pTxtDetailsLabels->setTextAlignment( EGUIA_LOWERRIGHT, EGUIA_UPPERLEFT );
+  m_pTxtDetails->setOverrideColor( SColor(255, 196, 196, 255) );
+	m_pTxtDetails->setOverrideFont(pFont);
+
+  // create the buttons
+  core::rect<s32> bottomRect = core::rect<s32>( 10, 85, rect.getWidth()-10, rect.getHeight()-10 );
+  core::rect<s32> btnRect = bottomRect;
+
+  btnRect.LowerRightCorner.Y = bottomRect.UpperLeftCorner.Y + 20;
+  btnRect.LowerRightCorner.X = bottomRect.UpperLeftCorner.X + 100;
+  environment->addButton(btnRect, this, 1, L"Transfer");
+
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -21,7 +46,12 @@ void GUIBankView::updateAccountDetails(BankAccount* pAccount)
   if ( !pAccount )
     return;
 
+  std::wstringstream ss;
 
+  ss << pAccount->getBalance() << "\n" << pAccount->getDebt() << "\n" << pAccount->getInterestRate() << "%\n" << (pAccount->isSecure() ? "Yes" : "No");
+
+  m_pTxtDetailsLabels->setText(L"Balance :\nDebt :\nInterest Rate :\nIs secure :\n");
+  m_pTxtDetails->setText(ss.str().c_str());
 }
 
 ///////////////////////////////////////////////////////////////////////
