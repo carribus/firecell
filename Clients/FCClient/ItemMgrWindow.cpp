ee0635e0dc750cb4801893702df13640f521ea22
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2010-03-18 19:48:57 +0000

    Added ItemMgrWindow object for managing a player's items (early shell of the class - no implementation)

---
+++ Clients/FCClient/ItemMgrWindow.cpp
@@ -0,0 +1,50 @@
+#include "../common/ResourceManager.h"
+#include "../../common/game_objects/ItemSoftware.h"
+#include "FCController.h"
+#include "FCViewEvent.h"
+#include "clientstrings.h"
+#include "ItemMgr.h"
+#include "ItemMgrWindow.h"
+
+DECLARE_FORM_ELEMENTS(ItemMgrWindow)
+  FORM_ELEMENT("staticText"			, 1							, 10			, 40			, 70			, 25			, L"")
+  FORM_ELEMENT("hyperlink"			, 3             , 85			, 44			, 250			, 20			, L"")
+  FORM_ELEMENT("checkBox"       , 50            , 340     , 40      , 25      , 25      , L"")
+  FORM_ELEMENT("vumeter"        , 51            , 380     , 40      , 125     , 10      , L"")
+  FORM_ELEMENT("staticText"     , 52            , 380     , 53      , 125     , 12      , L"")
+END_FORM_ELEMENTS();
+
+IMPLEMENT_IRRLICHT_FORM(ItemMgrWindow);
+
+///////////////////////////////////////////////////////////////////////
+
+ItemMgrWindow::ItemMgrWindow(IDesktop* pDesktop, FCModel& model, IGUIEnvironment* pEnv, wchar_t* caption, IGUIElement* pParent, s32 id)
+: FCDialog(pEnv, pParent, caption, false, id, rect<s32>(0, 0, 530, 340))
+, m_model(model)
+, m_pDesktop(pDesktop)
+, m_pController(NULL)
+{
+  createFormElements();
+
+  FCDialog::setSuccessCallback(OnOK, this);
+
+  CenterWindow();
+}
+
+///////////////////////////////////////////////////////////////////////
+
+ItemMgrWindow::~ItemMgrWindow(void)
+{
+}
+
+///////////////////////////////////////////////////////////////////////
+
+void ItemMgrWindow::OnOK(void* pCtx)
+{
+}
+
+///////////////////////////////////////////////////////////////////////
+
+void ItemMgrWindow::OnCancel(void* pCtx)
+{
+}
---
+++ Clients/FCClient/ItemMgrWindow.h
@@ -0,0 +1,36 @@
+#ifndef _ITEMMGRWINDOW_H_
+#define _ITEMMGRWINDOW_H_
+
+#include <string>
+#include "FCModel.h"
+#include "FCDialog.h"
+#include "IDesktop.h"
+
+class FCController;
+
+class ItemMgrWindow : public FCDialog
+{
+  DEFINE_IRRLICHT_FORM();
+
+public:
+  ItemMgrWindow(IDesktop* pDesktop, FCModel& model, IGUIEnvironment* pEnv, wchar_t* caption, IGUIElement* pParent, s32 id);
+  ~ItemMgrWindow(void);
+
+  void setController(FCController* pController)                         { m_pController = pController; }
+
+protected:
+	static void OnOK(void* pCtx);
+	static void OnCancel(void* pCtx);
+
+private:
+  /*
+   *  Private Members
+   */
+  FCModel&                m_model;
+  IrrlichtDevice*         m_pDevice;
+  IDesktop*               m_pDesktop;
+  FCController*           m_pController;
+
+};
+
+#endif//_ITEMMGRWINDOW_H_
