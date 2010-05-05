3fa69b5f3781b6f05a985453a8e68fc0e4326638
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2010-05-05 16:37:33 +0000

    Added Mac OS X specific .pro file and other Mac specific code changes

--- trunk/Clients/FCClient_Qt/ViewCharSelection.cpp
+++ trunk/Clients/FCClient_Qt/ViewCharSelection.cpp
@@ -1,3 +1,22 @@
+/*
+    FireCell Qt Client - The Qt client code for the firecell multiplayer game
+    Copyright (C) 2008-2010  Peter M. Mares
+
+                Contact: carribus@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "StdAfx.h"
 #include "clientstrings.h"
 #include "FCApp.h"
