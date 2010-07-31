10582e58b477e5db306ec89ec8b34642fcae6434
carribus <carribus@7ee4d4c8-7655-0410-a063-53603d3d390a>
2010-07-31 10:38:49 +0000

    Added ForumCategory :)

---
+++ Clients/FCClient_Qt/ForumCategory.cpp
@@ -0,0 +1,66 @@
+#ifdef _USE_STDAFX_H_
+  #include "StdAfx.h"
+#endif
+#include "ForumCategory.h"
+
+ForumCategory::ForumCategory(FCULONG id, FCULONG parent_id, FCULONG order, FCULONG thread_count, const QString& name, const QString& desc, ForumCategory* parent)
+: m_parent(parent)
+, m_id(id)
+, m_parentID(parent_id)
+, m_order(order)
+, m_threadCount(thread_count)
+, m_name(name)
+, m_desc(desc)
+{
+}
+
+///////////////////////////////////////////////////////////////////////
+
+ForumCategory::~ForumCategory(void)
+{
+  qDeleteAll(m_children);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+bool ForumCategory::addChild(ForumCategory* pCategory)
+{
+	if ( !pCategory )
+		return false;
+
+  m_children.push_back(pCategory);
+  pCategory->setParent(this);
+  return true;
+/*
+	ForumCategoryMap::iterator it = m_mapCategories.find( pCategory->getID() );
+
+	if ( it == m_mapCategories.end() )
+	{
+		m_mapCategories[ pCategory->getID() ] = pCategory;
+		pCategory->setParent(this);
+		return true;
+	}
+
+  return false;
+*/
+}
+
+///////////////////////////////////////////////////////////////////////
+
+ForumCategory* ForumCategory::childAt(int nIndex)
+{
+  if ( nIndex < 0 || nIndex >= m_children.count() )
+    return NULL;
+
+  return m_children.at(nIndex);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+int ForumCategory::currentIndex()
+{
+  if ( m_parent )
+    return m_parent->m_children.indexOf(const_cast<ForumCategory*>(this));
+
+  return 0;
+}
---
+++ Clients/FCClient_Qt/ForumCategory.h
@@ -0,0 +1,44 @@
+#ifndef _FORUMCATEGORY_H_
+#define _FORUMCATEGORY_H_
+
+#include "../../common/fctypes.h"
+#include <QList>
+
+class ForumCategory
+{
+public:
+  ForumCategory(FCULONG id, FCULONG parent_id, FCULONG order, FCULONG thread_count, const QString& name, const QString& desc, ForumCategory* parent);
+  ~ForumCategory(void);
+
+	bool addChild(ForumCategory* pCategory);
+  ForumCategory* childAt(int nIndex);
+  int currentIndex();
+  int childCount()                                { return m_children.count(); }
+
+	FCULONG getID()																	{ return m_id; }
+	FCULONG getParentID()														{ return m_parentID; }
+	FCULONG getOrder()															{ return m_order; }
+  FCULONG getThreadCount()                        { return m_threadCount; }
+	QString getName()														    { return m_name; }
+	QString getDesc()														    { return m_desc; }
+	ForumCategory* getParent()											{ return m_parent; }
+
+  void setOrder(FCULONG order)                    { m_order = order; }
+  void setThreadCount(FCULONG count)              { m_threadCount = count; }
+  void setName(const QString& name)               { m_name = name; }
+  void setDesc(const QString& desc)               { m_desc = desc; }
+	void setParent(ForumCategory* pParent)					{ m_parent = pParent; }
+
+private:
+
+  FCULONG                       m_id;
+  FCULONG                       m_parentID;
+  FCULONG                       m_order;
+  FCULONG                       m_threadCount;
+  QString                       m_name;
+  QString                       m_desc;
+  QList<ForumCategory*>         m_children;
+  ForumCategory*                m_parent;
+};
+
+#endif//_FORUMCATEGORY_H_
